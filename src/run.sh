39667b44bbb277775808f700bb8c738f145fa0c9
Kentaro Miki <mikiken.dev@gmail.com>
2023-10-26 01:25:44 +0900

    Complete Step5

--- src/run.sh
+++ src/run.sh
@@ -4,5 +4,15 @@ set -xue
 # QEMUの実行バイナリへのパス
 QEMU=qemu-system-riscv32
 
+CC=clang
+LINKER=ld.lld
+
+# コンパイルオプション
+CFLAGS="-c -std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib -mno-relax"
+
+# カーネルをビルド
+$CC $CFLAGS -o kernel.o kernel.c
+$LINKER -m elf32lriscv -L/lib -Tkernel.ld -Map=kernel.map kernel.o -o kernel.elf
+
 # QEMUを起動
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
