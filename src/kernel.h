6b23ca46db3f5753a8562d67f4163326d052d64c
Kentaro Miki <mikiken.dev@gmail.com>
2023-11-02 11:07:18 +0900

    Fix macro
    複数の命令を含むマクロは`do {...} while(0)`で囲わないと、if-else文の中とかで展開されたときにバグる場合があるので注意

--- src/kernel.h
+++ src/kernel.h
@@ -6,7 +6,7 @@ struct sbiret {
 };
 
 #define PANIC(fmt, ...)                                                       \
-    {                                                                         \
+    do {                                                                      \
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
         while (1) {}                                                          \
-    }
+    } while (0)
