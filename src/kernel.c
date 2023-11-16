3faa06ec8187043c10fec04c5f636dda7ca8a94b
Kentaro Miki <mikiken.dev@gmail.com>
2023-11-17 02:09:49 +0900

    Complete Step10

--- src/kernel.c
+++ src/kernel.c
@@ -28,6 +28,20 @@ void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); // console putchar
 }
 
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t)__free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t)__free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *)paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 __attribute__((naked))
 __attribute__((aligned(4))) void
 kernel_entry(void) {
@@ -122,6 +136,13 @@ void handle_trap(struct trap_frame *f) {
 void kernel_main(void) {
     printf("\n\nHello World!\n");
     printf("2 + 8 * 5 = %d, %x\n", 2 + 8 * 5, 0x1234abcd);
+
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=0x%x\n", paddr0);
+    printf("alloc_pages test: paddr1=0x%x\n", paddr1);
+
+    PANIC("booted!");
 
     WRITE_CSR(stvec, (uint32_t)kernel_entry);
     __asm__ __volatile("unimp"); // dummy instruction
