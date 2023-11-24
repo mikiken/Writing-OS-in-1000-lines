7f887d984364885f0c00c9c8cd6de560f994deb7
Kentaro Miki <mikiken.dev@gmail.com>
2023-11-25 01:24:13 +0900

    Impl shell

--- src/shell.c
+++ src/shell.c
@@ -1,5 +1,31 @@
 #include "user.h"
 
 void main(void) {
-    printf("🎉 Hello, world from shell! 🎉\n");
+    while (1) {
+    prompt:
+        printf("> ");
+        char cmdline[128];
+
+        for (int i = 0;; i++) {
+            char ch = getchar();
+            putchar(ch);
+            if (i == sizeof(cmdline) - 1) {
+                printf("command line too long\n");
+                goto prompt;
+            }
+            else if (ch == '\r') {
+                printf("\n");
+                cmdline[i] = '\0';
+                break;
+            }
+            else {
+                cmdline[i] = ch;
+            }
+        }
+
+        if (strcmp(cmdline, "hello") == 0)
+            printf("Hello world from shell!\n");
+        else
+            printf("unknown command: %s\n", cmdline);
+    }
 }
