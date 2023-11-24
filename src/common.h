08339d2219fbb3bc056a8062aea5878834f81bb4
Kentaro Miki <mikiken.dev@gmail.com>
2023-11-25 01:25:27 +0900

    Format code

--- src/common.h
+++ src/common.h
@@ -14,14 +14,18 @@ typedef uint32_t paddr_t;
 typedef uint32_t vaddr_t;
 
 #define NULL ((void *)0)
+
 #define align_up(value, align) __builtin_align_up(value, align_up)
 #define is_aligned(value, align) __builtin_is_aligned(value, align)
 #define offsetof(type, member) __builtin_offsetof(type, member)
+
 #define va_list __builtin_va_list
 #define va_start __builtin_va_start
 #define va_end __builtin_va_end
 #define va_arg __builtin_va_arg
+
 #define PAGE_SIZE 4096
+
 #define SYS_PUTCHAR 1
 #define SYS_GETCHAR 2
 
