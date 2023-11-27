f9163f0b108db701ab0705ac08efd9e2fff82448
Kentaro Miki <mikiken.dev@gmail.com>
2023-11-27 11:54:21 +0900

    Fix typo

--- src/common.h
+++ src/common.h
@@ -15,7 +15,7 @@ typedef uint32_t vaddr_t;
 
 #define NULL ((void *)0)
 
-#define align_up(value, align) __builtin_align_up(value, align_up)
+#define align_up(value, align) __builtin_align_up(value, align)
 #define is_aligned(value, align) __builtin_is_aligned(value, align)
 #define offsetof(type, member) __builtin_offsetof(type, member)
 
