eb37bc06821adea6f52615a9fbf884a808301c4c
Kentaro Miki <mikiken.dev@gmail.com>
2023-12-07 22:39:41 +0900

    Impl filesystem (read)

--- src/kernel.c
+++ src/kernel.c
@@ -332,6 +332,45 @@ void read_write_disk(void *buf, unsigned sector, bool is_write) {
         memcpy(buf, blk_req->data, SECTOR_SIZE);
 }
 
+struct file files[FILES_MAX];
+uint8_t disk[DISK_MAX_SIZE];
+
+int oct2int(char *oct, int len) {
+    int dec = 0;
+    for (int i = 0; i < len; i++) {
+        if (oct[i] < '0' || oct[i] > '7')
+            break;
+
+        dec = dec * 8 + (oct[i] - '0');
+    }
+    return dec;
+}
+
+void fs_init(void) {
+    for (unsigned sector = 0; sector < sizeof(disk) / SECTOR_SIZE; sector++)
+        read_write_disk(&disk[sector * SECTOR_SIZE], sector, false);
+
+    unsigned offset = 0;
+    for (int i = 0; i < FILES_MAX; i++) {
+        struct tar_header *header = (struct tar_header *)&disk[offset];
+        if (header->name[0] == '\0')
+            break;
+
+        if (strcmp(header->magic, "ustar") != 0)
+            PANIC("invalid tar header: magic=\"%s\"", header->magic);
+
+        int file_size = oct2int(header->size, sizeof(header->size));
+        struct file *file = &files[i];
+        file->in_use = true;
+        strcpy(file->name, header->name);
+        memcpy(file->data, header->data, file_size);
+        file->size = file_size;
+        printf("file: %s, size=%d\n", file->name, file->size);
+
+        offset += align_up(sizeof(struct tar_header) + file_size, SECTOR_SIZE);
+    }
+}
+
 __attribute__((naked))
 __attribute__((aligned(4))) void
 kernel_entry(void) {
@@ -467,6 +506,7 @@ void kernel_main(void) {
     memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
     WRITE_CSR(stvec, (uint32_t)kernel_entry);
     virtio_blk_init();
+    fs_init();
 
     char buf[SECTOR_SIZE];
     read_write_disk(buf, 0, false);
